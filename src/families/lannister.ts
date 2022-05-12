ea447e24966d508109a9fbfd43188734d23b965b
temlabs <temilabinjo@hotmail.co.uk>
2022-05-12 10:48:57 +0100

    Lannister family completed

--- src/families/lannister.ts
+++ src/families/lannister.ts
@@ -1,6 +1,27 @@
 import { Person } from "../personTypes";
 
 export function createLannisterTree(): Person {
-    //https://en.wikipedia.org/wiki/Template:Family_tree_of_Tytos_Lannister
-    return { name: "LANNISTER: YOU NEED TO IMPLEMENT THIS", children: [] };
+
+    const cerseiChildren: Person[] = [
+        { name: "Joffrey", children: [] },
+        { name: "Myrcella", children: [] },
+        { name: "Tommen", children: [] }
+    ]
+
+    const tywinChildren: Person[] = [
+        { name: "Cersei", children: cerseiChildren },
+        { name: "Ser Jaime", children: [] },
+        { name: "Tyrion", children: [] }
+    ]
+
+    const tytosChildren: Person[] = [
+        { name: "Tywin", children: tywinChildren },
+        { name: "Genna", children: [] },
+        { name: "Kevan", children: [] },
+        { name: "Tygett", children: [] },
+        { name: "Gerion", children: [] }
+    ]
+
+    const topPerson: Person = { name: "Tytos", children: tytosChildren };
+    return topPerson;
 }
