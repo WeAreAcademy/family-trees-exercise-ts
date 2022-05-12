54669980e34a8814964a8acd88cfb499c354e2d4
Owen Dearman <owendearman@gmail.com>
2022-05-12 10:36:09 +0100

    House Stark

--- src/families/stark.ts
+++ src/families/stark.ts
@@ -2,5 +2,22 @@ import { Person } from "../personTypes";
 
 export function createStarkTree(): Person {
 
-    return { name: "STARK: YOU NEED TO IMPLEMENT THIS", children: [] };
+    const eddardsChildren: Person[] = [
+        {name: "Rickon", children: []},
+        {name: "Bran", children: []},
+        {name: "Arya", children: []},
+        {name: "Sansa", children: []},
+        {name: "Robb", children: []},
+        {name: "Jon", children: []},
+    ] 
+
+    const rickardsChildren: Person[] = [
+        {name: "Benjen", children: []},
+        {name: "Lyanna", children: []},
+        {name: "Eddard", children: eddardsChildren},
+        {name: "Brandon", children: []}
+    ]
+
+    const topPerson: Person = {name: "Rickard", children: rickardsChildren}
+    return topPerson;
 }
