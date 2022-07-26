a2c30db8465cddee184823aceb38b7da864a2b43
Bhawick Jain <bhawick@gmail.com>
2022-07-26 10:54:39 +0100

    initial commit for task

--- src/families/stark.ts
+++ src/families/stark.ts
@@ -1,6 +1,24 @@
 import { Person } from "../personTypes";
 
 export function createStarkTree(): Person {
+  const eddardChildren: Person[] = [
+    { name: "Rickon", children: [] },
+    { name: "Bran", children: [] },
+    { name: "Arya", children: [] },
+    { name: "Sansa", children: [] },
+    { name: "Robb", children: [] },
+    { name: "Jon", children: [] },
+  ];
 
-    return { name: "STARK: YOU NEED TO IMPLEMENT THIS", children: [] };
+  const topPerson: Person = {
+    name: "Rickard",
+    children: [
+      { name: "Benjen", children: [] },
+      { name: "Lyanna", children: [] },
+      { name: "Eddard", children: eddardChildren },
+      { name: "Brandon", children: [] },
+    ],
+  };
+
+  return topPerson;
 }
