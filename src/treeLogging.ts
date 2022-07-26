d313dd899486a9291f6290d8e0520a6940c40022
Bhawick Jain <bhawick@gmail.com>
2022-07-26 10:53:27 +0100

    implement FILO stackArray

--- src/treeLogging.ts
+++ src/treeLogging.ts
@@ -1,11 +1,33 @@
 import { Person } from "./personTypes";
 
+/*
+FUNCTION: logAllPeopleInTree
+PARAMS: topPerson: Person
+RETURNS: VOID
+----
+DECLARE stackArr: ARRAY OF PERSON = []
+stackArr.PUSH(topPerson)
 
+WHILE (stackArr.LENGTH > 0)
+  DECLARE topPerson = stackArr.POP
+  PRINT(topPerson)
+  FOR (child IN topPerson.children)
+    stackArr.PUSH(child)
+  ENDFOR
+ENDWHILE
+----
+*/
 export function logAllPeopleInTree(topPerson: Person): void {
-    console.log("TODO!  First person is " + topPerson.name);
+  const stackArr: Person[] = [];
+  stackArr.unshift(topPerson);
+
+  while (stackArr.length > 0) {
+    const topPerson = stackArr.pop();
+    console.log(topPerson?.name);
+    topPerson?.children.forEach((child) => stackArr.unshift(child));
+  }
 }
 
 export function logAllPeopleInTreeWithQueue(topPerson: Person): void {
-    console.log("TODO!  First person is " + topPerson.name);
+  console.log("TODO!  First person is " + topPerson.name);
 }
-
