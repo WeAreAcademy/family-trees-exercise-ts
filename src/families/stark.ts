ae857644f7469b85024b5b11f28ec74b7aedfaf3
Laura Hall <podinjshmi1993@gmail.com>
2023-07-24 10:53:41 +0100

    eddaers kids added

--- src/families/stark.ts
+++ src/families/stark.ts
@@ -2,5 +2,21 @@ import { Person } from "../personTypes";
 
 export function createStarkTree(): Person {
 
-    return { name: "Richard", children: [] };
+const eddardsKids : Person[] = [
+    {name: "Rickon" , children : []},
+    {name: "Bran" , children : []},
+    {name: "Arya" , children : []},
+    {name: "Sansa" , children : []},
+    {name: "Robb" , children : []},
+    {name: "Jon" , children : []},
+]
+
+const richardsKids : Person[] = [
+    {name: "Benjen" , children : []},
+    {name: "Lyanna" , children : []},
+    {name: "Eddard" , children : eddardsKids},
+    {name: "Brandon" , children : []},
+]
+
+    return { name: "Richard", children: ["Eddard", ] };
 }
